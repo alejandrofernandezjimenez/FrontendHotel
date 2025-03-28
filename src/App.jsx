fff5ab10c147e537db22142186eb8ebbd0bc4df8
pablogonzalezamores <pablo.gonzalezamores@plexus.es>
2025-03-28 08:36:05 +0100

    PeticionInfinita

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReservaForm from './components/ReservaForm';
+import PeticionInfinita from './components/PeticionInfinita';
 
 function App() {
   return (
     <div className="App">
       <h1>Gestión de Reservas</h1>
       <ReservaForm />
+      <PeticionInfinita/>
     </div>
   );
 }
---
+++ src/components/PeticionInfinita.jsx
@@ -0,0 +1,23 @@
+const PeticionInfinita = () => {
+    async function obtenerReservas() {
+        try {
+          const response = await fetch("https://crudreservas.onrender.com/api/reservas/listar");
+          if (!response.ok) {
+            throw new Error("Error en la solicitud: " + response.status);
+          }
+          const data = await response.json();
+          console.log("Datos recibidos:", data);
+        } catch (error) {
+          console.error("Hubo un problema con la solicitud:", error);
+        }
+      }
+      
+      // Ejecutar inmediatamente al inicio
+      obtenerReservas();
+      
+      // Luego repetir cada 3 minutos (180000 milisegundos)
+      setInterval(obtenerReservas, 180000);
+      
+}
+
+export default PeticionInfinita;
